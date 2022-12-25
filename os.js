84661e0c4a1d1835a22dfb9e4381a23818b34e64
Omkar Dounde <113289561+OmkarDounde@users.noreply.github.com>
2022-12-25 18:18:06 +0530

    Add files via upload

---
+++ os.js
@@ -0,0 +1,8 @@
+let os = require('os');
+console.log(os.platform())
+console.log(os.arch())
+console.log(os.freemem())
+console.log(os.uptime())
+console.log(os.platform())
+console.log(os.cpus())
+console.log(`${os.cpus().length} Core`)
